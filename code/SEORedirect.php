8af0f767488ff7202196b2e4da4cca144e2aa2e6
Thomas B. Nielsen <mail@nobrainer.dk>
2013-01-17 15:55:59 +0100

    Fixed indents and some formatting

--- code/SEORedirect.php
+++ code/SEORedirect.php
@@ -9,65 +9,65 @@
 
 class SEORedirect extends DataObjectDecorator{
 	/**
-	 * Method to construct
-	 */
+	* Method to construct
+	*/
 	public function __construct(){
 		parent::__construct();
 	}
 	
 	public function extraStatics($class = null, $extension = null) {
 		return array(
-            'has_many' => array(
-                'SEORedirectUrls' => 'SEORedirectUrl'
-            )
-        );
+			'has_many' => array(
+				'SEORedirectUrls' => 'SEORedirectUrl'
+			)
+		);
 	}
 	
 	/**
-	 * Update CMS fields
-	 */
+	* Update CMS fields
+	*/
 	public function updateCMSFields(FieldList $fields){
 		//Redirect tabs
 		$Redirect = new ComplexTableField(
 			$this,
-	    	'SEORedirectUrls',
-	    	'SEORedirectUrl',
-	    	array(
-				'Source' 		    => 'Source',
+	 	'SEORedirectUrls',
+	 	'SEORedirectUrl',
+	 	array(
+				'Source' 		 => 'Source',
 				'TargetLink' 		=> 'Target link',
-                'TargetPage.Title'  => 'Target page',
-                'IsActive'  		=> 'Active',
-	    	),
-	    	'getCMSFields_forPopup',
+				'TargetPage.Title' => 'Target page',
+				'IsActive' 		=> 'Active',
+	 	),
+	 	'getCMSFields_forPopup',
 			"\"TargetPageID\" = '".$this->owner->ID."' "
 		);
 		
 		$Redirect->setAddTitle('an url');
 		$Redirect->popupClass = 'SEORedirect_Popup';
-        
-        $fields->addFieldToTab('Root.Content.Redirect', $Redirect);
+		
+		$fields->addFieldToTab('Root.Content.Redirect', $Redirect);
 	}
 	
 	
 	/**
-	 * Method to handle request
-	 */
+	* Method to handle request
+	*/
 	public static function handleRequest($request){
 		//get request variables
-		$baseURL    = str_replace('index.php/', '', Director::BaseURL());
+		$baseURL	= str_replace('index.php/', '', Director::BaseURL());
 		$requestURI = str_replace('index.php/', '', $_SERVER['REQUEST_URI']);
-        
-        if( $baseURL  == '' || $baseURL == '/' ){
-            $link  = trim($requestURI, '/');
-        }else{
-            $link  = rtrim(str_replace($baseURL, '', $requestURI), '/');
-        }
-        
-        if( $target = self::get_by_link($link)  ){
+		
+		if( $baseURL == '' || $baseURL == '/' ){
+			$link = trim($requestURI, '/');
+		}else{
+			$link = rtrim(str_replace($baseURL, '', $requestURI), '/');
+		}
+		
+		if( $target = self::get_by_link($link) ){
 			$TargetLink = $target->TargetLink ? $target->TargetLink :
 				( $target->TargetPage()->Exists() ? str_replace('/index.php/', '/',$target->TargetPage()->Link()) : null );
-                
-            if($TargetLink){
+				
+			if($TargetLink){
 				//Forward parameters to target
 				if( isset($target->ForwardParameters) && $target->ForwardParameters ){
 					$url_data = parse_url($link);
@@ -94,11 +94,11 @@ class SEORedirect extends DataObjectDecorator{
 	}
 	
 	/**
-	 * Returns a link mapping for a link if one exists.
-	 *
-	 * @param  string $link
-	 * @return LinkMapping
-	 */
+	* Returns a link mapping for a link if one exists.
+	*
+	* @param string $link
+	* @return LinkMapping
+	*/
 	public static function get_by_link($link) {
 		return DataObject::get_one('SEORedirectUrl', " MD5(`Source`) = '".md5( strtolower( trim($link) ) )."' AND `Active` = 1 ");
 	}
@@ -109,26 +109,26 @@ class SEORedirect extends DataObjectDecorator{
  */
 class SEORedirectUrl extends DataObject{
 	/**
-	 * Human-readable singular name.
-	 * @var string
-	 */
+	* Human-readable singular name.
+	* @var string
+	*/
 	public static $singular_name = 'url';
 	
 	/**
-	 * Human-readable pluaral name
-	 * @var string
-	 */
+	* Human-readable pluaral name
+	* @var string
+	*/
 	public static $plural_name = 'urls';
 	
 	/**
-	 * Database fields
-	 */
+	* Database fields
+	*/
 	public static $db = array(
-		'Source'     		=> 'Varchar(300)',
-		'TargetLink' 		=> 'Varchar(300)',
-		'Code'       		=> 'Enum("301, 302", "301")',
-		'Active'     	    => 'Boolean(1)',
-		'ForwardParameters' => 'Boolean(0)'
+		'Source'			=> 'Varchar(300)',
+		'TargetLink'		=> 'Varchar(300)',
+		'Code'				=> 'Enum("301, 302", "301")',
+		'Active'			=> 'Boolean(1)',
+		'ForwardParameters'	=> 'Boolean(0)'
 	);
 
 	public static $has_one = array(
@@ -157,11 +157,11 @@ class SEORedirectUrl extends DataObject{
 	*/
 
 	/**
-	 * Returns a link mapping for a link if one exists.
-	 *
-	 * @param  string $link
-	 * @return SEORedirectUrl
-	 */
+	* Returns a link mapping for a link if one exists.
+	*
+	* @param string $link
+	* @return SEORedirectUrl
+	*/
 	public static function get_by_link($link) {
 		return DataObject::get_one('SEORedirectUrl', sprintf(
 			'"Source" = \'%s\'', Convert::raw2sql(self::unify_link($link))
@@ -169,11 +169,11 @@ class SEORedirectUrl extends DataObject{
 	}
 
 	/**
-	 * Unifies a link so mappings are predictable.
-	 *
-	 * @param  string $link
-	 * @return string
-	 */
+	* Unifies a link so mappings are predictable.
+	*
+	* @param string $link
+	* @return string
+	*/
 	public static function unify_link($link) {
 		return strtolower(trim(Director::makeRelative(strtok($link, '?')), '/'));
 	}
@@ -181,8 +181,7 @@ class SEORedirectUrl extends DataObject{
 	/*
 	* Method to show CMS fields for Popup for creating or updating
 	*/
-	public function getCMSFields_forPopup()
-	{
+	public function getCMSFields_forPopup() {
 		//init variables
 		$fields = new FieldSet();
 		
@@ -195,22 +194,22 @@ class SEORedirectUrl extends DataObject{
 		$target->setRightTitle('External url which redirects to, leave blank to redirect to current page');
 		$fields->push($target);
 		
-        $fields->push( new DropdownField('Code', 'Redirect code', $this->dbObject('Code')->enumValues()) );
+		$fields->push( new DropdownField('Code', 'Redirect code', $this->dbObject('Code')->enumValues()) );
 		
 		return $fields;
 	}
 	
 	/**
-	 * On before save record
-	 */
+	* On before save record
+	*/
 	public function onBeforeWrite(){
 		$this->Source = strtolower( ltrim( rtrim( trim($this->Source), '/'), '/' ) );
-        
-        if( $this->TargetLink ){
+		
+		if( $this->TargetLink ){
 			$this->TargetLink = strtolower( trim($this->TargetLink) );
 		}
-        
-        parent::onBeforeWrite();
+		
+		parent::onBeforeWrite();
 	}
 }
 
@@ -219,8 +218,8 @@ class SEORedirectUrl extends DataObject{
  */
 class SEORedirect_Popup extends ComplexTableField_Popup{
 	/**
-	 * Construct
-	 */
+	* Construct
+	*/
 	public function __construct($controller, $name, $fields, $validator, $readonly, $dataObject){
 		//extends validator
 		$validator = new SEORedirectUrl_Validator( array('Source') );
@@ -234,18 +233,18 @@ class SEORedirect_Popup extends ComplexTableField_Popup{
  */
 class SEORedirectUrl_Validator extends RequiredFields{
 	/**
-	 * server-side validator
-	 */
+	* server-side validator
+	*/
 	public function php($data){
-		$form    = $this->form;
-		$valid   = parent::php($data);
-		$regex   = '/^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i';
+		$form	= $this->form;
+		$valid = parent::php($data);
+		$regex = '/^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i';
 		
-        $source  = strtolower( ltrim(rtrim(trim($data['Source']),'/'),'/') );
-        
+		$source = strtolower( ltrim(rtrim(trim($data['Source']),'/'),'/') );
+		
 		/**
-		 * Ensure that the source is unique
-		 */
+		* Ensure that the source is unique
+		*/
 		if($valid){
 			$query = new SQLQuery();
 			$where = null;
@@ -266,18 +265,18 @@ class SEORedirectUrl_Validator extends RequiredFields{
 				$valid = false;
 			}
 		}
-        
-        /**
-         * Ensure that the source does not match with any URLSegments
-         */
-        if($valid){
-            $query = new SQLQuery();
-            $res   = false;
-            
-            $res = self::URLSegmentExists($query, 0, explode('/', $source) );
-            
-            //$res = DataObject::get_one('SiteTree', " LOWER(`URLSegment`) = '".$source."' ");
-            
+		
+		/**
+		 * Ensure that the source does not match with any URLSegments
+		 */
+		if($valid){
+			$query = new SQLQuery();
+			$res = false;
+			
+			$res = self::URLSegmentExists($query, 0, explode('/', $source) );
+			
+			//$res = DataObject::get_one('SiteTree', " LOWER(`URLSegment`) = '".$source."' ");
+			
 			if($res){
 				$this->validationError(
 					"Source", "Source pattern is matched with a page URLSegment", "required"
@@ -286,11 +285,11 @@ class SEORedirectUrl_Validator extends RequiredFields{
 				$valid = false;
 			}
 		}
-        
+		
 		
 		/**
-		 * Validate external url if it is given
-		 */
+		* Validate external url if it is given
+		*/
 		if( $valid && isset($data['TargetLink']) && $data['TargetLink'] != '' && ! (bool)preg_match($regex, $data['TargetLink']) ){
 			$this->validationError(
 				"TargetLink", "Please fill in a valid url", "required"
@@ -301,33 +300,33 @@ class SEORedirectUrl_Validator extends RequiredFields{
 		
 		return $valid;
 	}
-    
-    
-    /**
-     * check if URLSegment is already exist or not
-     */
-    protected function URLSegmentExists($SQLQuery, $ParentID = 0, $URLSegments = array()){
-        if( count($URLSegments) > 1 ){
-            $URLSegments = array_values($URLSegments);
-            
-            if( isset($URLSegments[0]) ){
-                $res = DataObject::get_one('SiteTree', " LOWER(`URLSegment`) = '".$URLSegments[0]."' AND ParentID = ".(int)$ParentID);
-                
-                unset($URLSegments[0]);
-                
-                if($res){
-                    return self::URLSegmentExists($SQLQuery, $res->ParentID, $URLSegments);
-                }
-            }
-        }
-        
-        if( count($URLSegments) == 1 ){
-            $URLSegments = array_values($URLSegments);
-            
-            return DataObject::get_one('SiteTree', " LOWER(`URLSegment`) = '".$URLSegments[0]."' ");
-        }
-        
-        return false;
-    }
+	
+	
+	/**
+	 * check if URLSegment is already exist or not
+	 */
+	protected function URLSegmentExists($SQLQuery, $ParentID = 0, $URLSegments = array()){
+		if( count($URLSegments) > 1 ){
+			$URLSegments = array_values($URLSegments);
+			
+			if( isset($URLSegments[0]) ){
+				$res = DataObject::get_one('SiteTree', " LOWER(`URLSegment`) = '".$URLSegments[0]."' AND ParentID = ".(int)$ParentID);
+				
+				unset($URLSegments[0]);
+				
+				if($res){
+					return self::URLSegmentExists($SQLQuery, $res->ParentID, $URLSegments);
+				}
+			}
+		}
+		
+		if( count($URLSegments) == 1 ){
+			$URLSegments = array_values($URLSegments);
+			
+			return DataObject::get_one('SiteTree', " LOWER(`URLSegment`) = '".$URLSegments[0]."' ");
+		}
+		
+		return false;
+	}
 
 }
